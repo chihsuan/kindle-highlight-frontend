19dedcdc9e779f01cf8daa6567277c056d118572
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2021-04-29 20:38:33 +0800

    feat: API_HOST from env

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const API_HOST = 'http://localhost:8080/api/v1'
+export const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:8080/api/v1'
 export const GOOGLE_CLINET_ID = "1049134808087-43nr9ct4fusgm5nu9oth1bgq3p4ril1h.apps.googleusercontent.com"
 export const CONTACT_FROM_URI = 'https://script.google.com/macros/s/AKfycbx84YaiYO8AXTy532deRKzb2aEu3JrzmbnwLETX58MS1DW586KB4VDz7PaJh7OC3C32/exec';
 export const WEBSITE_TITLE = 'Kindle Highlight'
