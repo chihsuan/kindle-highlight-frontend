fbb690ca3022aedc1f307db32f3b15a5ff42964c
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2021-05-18 11:33:30 +0800

    feat: add made by info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import Head from 'next/head'
 import { Provider } from 'use-http'
 import type { AppProps } from 'next/app'
-import { ChakraProvider, Flex, Text } from "@chakra-ui/react"
+import { ChakraProvider, Flex, Link, Text } from "@chakra-ui/react"
 import { API_HOST, WEBSITE_TITLE } from "../constants"
 import { useTokenState } from "../data/persist"
 import theme from "../constants/theme"
@@ -28,6 +28,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <Component {...pageProps} />
         <Flex height="100" padding="24px" alignItems="center">
           <Text fontSize="sm" color="gray.600">©2021 {WEBSITE_TITLE}</Text>
+          <Text fontSize="sm" color="gray.600" marginLeft={1}>| Made By <Link href="https://github.com/chihsuan/kindle-highlight-frontend">@chihsuan</Link></Text>
         </Flex>
       </ChakraProvider>
     </Provider>
